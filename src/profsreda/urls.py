cf0609e0da887cb726fc0a0c3850c6b6bf7d6b84
Xomielanne <demidafonichev@gmail.com>
2018-07-19 15:59:16 +0500

    Commit defore push origin

--- src/profsreda/urls.py
+++ src/profsreda/urls.py
@@ -21,14 +21,14 @@ from django.urls import path, include
 from heroes import views as hero_view
 
 urlpatterns = [
-                  path('', hero_view.home),
-                  path('accounts/', include('allauth.urls')),
-                  path('admin/', admin.site.urls),
-                  path('profile/', hero_view.profile),
-                  path('profile/random/', hero_view.profile_random),
-                  path('accounts/profile/', hero_view.profile, name="account_profile"),
-                  path('accounts/profile/random/', hero_view.profile_random),
-                  path('accounts/profile/item/<int:item_pk>/', hero_view.profile_item),
-                  path('signup/', hero_view.customProfileCreation, name="signup"),
-                  path("tests/", include("tests.urls")),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', hero_view.home),
+    path('accounts/', include('allauth.urls')),
+    path('admin/', admin.site.urls),
+    path('profile/', hero_view.profile),
+    path('profile/random/', hero_view.profile_random),
+    path('accounts/profile/', hero_view.profile, name="account_profile"),
+    path('accounts/profile/random/', hero_view.profile_random),
+    path('accounts/profile/item/<int:item_pk>/', hero_view.profile_item),
+    path('signup/', hero_view.customProfileCreation, name="signup"),
+    path("tests/", include("tests.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
