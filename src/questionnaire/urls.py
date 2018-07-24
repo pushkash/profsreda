dc311a1e966c487a5f0694bae8baee57e8487e37
ozvairon <vairon3004@gmail.com>
2018-07-24 04:07:12 +0300

    clean repo

--- src/questionnaire/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path(r"get_questionnaire/<int:questionnaire_id>", views.get_questionnaire)
-]
--- src/questionnaire_responses/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'questionnaire_responses.apps.QuestionnaireResponsesConfig'
